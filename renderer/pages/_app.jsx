f6c270f9384adfc75e5e959b7ffbfced0f2eb53f
Parshuram Saini <Parshuram.saini@pravisht.com>
2026-01-09 17:32:48 +0530

    first scratch app with sample electron

---
+++ renderer/pages/_app.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Head from 'next/head'
+import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import theme from '../lib/theme'
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props
+  return (
+    <AppCacheProvider {...props}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </AppCacheProvider>
+  )
+}

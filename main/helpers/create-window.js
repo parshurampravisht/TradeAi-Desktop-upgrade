170bf0dbe94a4867096afbd500acc51624612d3d
Parshuram Saini <Parshuram.saini@pravisht.com>
2026-01-09 18:18:27 +0530

    fixed the issue

--- main/helpers/create-window.js
+++ main/helpers/create-window.js
@@ -1,18 +1,16 @@
 import {
   screen,
   BrowserWindow,
-  BrowserWindowConstructorOptions,
-  Rectangle,
 } from 'electron'
 import Store from 'electron-store'
 
 export const createWindow = (
-  windowName: string,
-  options: BrowserWindowConstructorOptions
-): BrowserWindow => {
+  windowName,
+  options
+) => {
   const key = 'window-state'
   const name = `window-state-${windowName}`
-  const store = new Store<Rectangle>({ name })
+  const store = new Store({ name })
   const defaultSize = {
     width: options.width,
     height: options.height,
--- main/preload.js
+++ main/preload.js
@@ -1,20 +1,21 @@
-import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron'
+const { contextBridge, ipcRenderer } = require("electron");
 
 const handler = {
-  send(channel: string, value: unknown) {
-    ipcRenderer.send(channel, value)
+  send(channel, value) {
+    ipcRenderer.send(channel, value);
   },
-  on(channel: string, callback: (...args: unknown[]) => void) {
-    const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
-      callback(...args)
-    ipcRenderer.on(channel, subscription)
+
+  on(channel, callback) {
+    const subscription = (_event, ...args) => {
+      callback(...args);
+    };
+
+    ipcRenderer.on(channel, subscription);
 
     return () => {
-      ipcRenderer.removeListener(channel, subscription)
-    }
+      ipcRenderer.removeListener(channel, subscription);
+    };
   },
-}
+};
 
-contextBridge.exposeInMainWorld('ipc', handler)
-
-export type IpcHandler = typeof handler
+contextBridge.exposeInMainWorld("ipc", handler);
